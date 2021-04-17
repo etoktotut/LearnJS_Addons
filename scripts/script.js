14b02e9f44f518511e7be827a1e657c375b096de
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-17 15:52:14 +0300

    HT14_1 complex

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const DomElement = function (selector, options) {
     this.selector = selector;
     options = options || {};
@@ -25,6 +24,41 @@ DomElement.prototype.elemCreate = function (strToElem) {
     el.textContent = strToElem;
     document.querySelector('body').insertAdjacentElement('afterbegin', el);
 };
+const elem1 = new DomElement('#fufu', { height: '100px', width: '100px', bg: 'red', fontSize: '30px' });
 
-const first = new DomElement('#fufu', { height: '10%', width: '40%', bg: 'green', fontSize: '30px' });
-first.elemCreate('Гога!');
+document.addEventListener('DOMContentLoaded', function () {
+    elem1.elemCreate();
+    const elem2 = document.querySelector('#fufu');
+    elem2.style.position = 'absolute';
+    let x = 0;
+    let y = 0;
+    window.addEventListener("keydown", function (event) {
+        if (event.defaultPrevented) {
+            console.log('something Wrong');
+            return; // Do nothing if the event was already processed
+        }
+        switch (event.key) {
+            case "ArrowDown":
+                y += 10;
+                elem2.style.transform = `translate(${x}px,${y}px)`;
+                break;
+            case "ArrowUp":
+                y -= 10;
+                elem2.style.transform = `translate(${x}px,${y}px)`;
+                break;
+            case "ArrowLeft":
+                x -= 10;
+                elem2.style.transform = `translate(${x}px,${y}px)`;
+                break;
+            case "ArrowRight":
+                x += 10;
+                elem2.style.transform = `translate(${x}px,${y}px)`;
+                break;
+            default:
+                return; // Quit when this doesn't handle the key event.
+        }
+    });
+    // elem2.addEventListener('')
+
+
+});
