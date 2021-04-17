3d52907a8cc138c356c80eda6fc659f3951631a5
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-17 11:03:27 +0300

    HT14 part 1

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const DomElement = function (selector, options) {
+    this.selector = selector;
+    options = options || {};
+    this.height = options.height;
+    this.width = options.width;
+    this.bg = options.bg;
+    this.fontSize = options.fontSize;
+};
+
+DomElement.prototype.elemCreate = function (strToElem) {
+    let el;
+    if (this.selector.charAt(0) === '.') {
+        el = document.createElement('div');
+        el.className = this.selector.substring(1);
+    } else
+        if (this.selector.charAt(0) === '#') {
+            el = document.createElement('p');
+            el.setAttribute('id', this.selector.substring(1));
+        } else {
+            return;
+        }
+    el.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`;
+    el.textContent = strToElem;
+    document.querySelector('body').insertAdjacentElement('afterbegin', el);
+};
+
+const first = new DomElement('#fufu', { height: '10%', width: '40%', bg: 'green', fontSize: '30px' });
+first.elemCreate('Гога!');
