4c0cba97ef6f401c512841e6fe3da2915b272e89
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-17 21:45:36 +0300

    HT14 complex corrected 1

--- scripts/script.js
+++ scripts/script.js
@@ -10,26 +10,18 @@ const DomElement = function (selector, options) {
 
 DomElement.prototype.elemCreate = function (strToElem) {
     let el;
-    if (this.selector.charAt(0) === '.') {
+    if (this.selector[0] === '.') {
         el = document.createElement('div');
         el.className = this.selector.substring(1);
-    } else
-        if (this.selector.charAt(0) === '#') {
-            el = document.createElement('p');
-            el.setAttribute('id', this.selector.substring(1));
-        } else {
-            return;
-        }
+    }
+    if (this.selector[0] === '#') {
+        el = document.createElement('p');
+        el.setAttribute('id', this.selector.substring(1));
+    }
     el.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`;
     el.textContent = strToElem;
+    el.style.position = 'absolute';
     document.querySelector('body').insertAdjacentElement('afterbegin', el);
-};
-const elem1 = new DomElement('#fufu', { height: '100px', width: '100px', bg: 'red', fontSize: '30px' });
-
-document.addEventListener('DOMContentLoaded', function () {
-    elem1.elemCreate();
-    const elem2 = document.querySelector('#fufu');
-    elem2.style.position = 'absolute';
     let x = 0;
     let y = 0;
     window.addEventListener("keydown", function (event) {
@@ -40,25 +32,28 @@ document.addEventListener('DOMContentLoaded', function () {
         switch (event.key) {
             case "ArrowDown":
                 y += 10;
-                elem2.style.transform = `translate(${x}px,${y}px)`;
+                el.style.transform = `translate(${x}px,${y}px)`;
                 break;
             case "ArrowUp":
                 y -= 10;
-                elem2.style.transform = `translate(${x}px,${y}px)`;
+                el.style.transform = `translate(${x}px,${y}px)`;
                 break;
             case "ArrowLeft":
                 x -= 10;
-                elem2.style.transform = `translate(${x}px,${y}px)`;
+                el.style.transform = `translate(${x}px,${y}px)`;
                 break;
             case "ArrowRight":
                 x += 10;
-                elem2.style.transform = `translate(${x}px,${y}px)`;
+                el.style.transform = `translate(${x}px,${y}px)`;
                 break;
             default:
                 return; // Quit when this doesn't handle the key event.
         }
     });
-    // elem2.addEventListener('')
+};
 
+const elem1 = new DomElement('#fufu', { height: '100px', width: '100px', bg: 'red', fontSize: '30px' });
+document.addEventListener('DOMContentLoaded', function () {
+    elem1.elemCreate();
 
 });
