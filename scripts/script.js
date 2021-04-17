2deca0f0cb43ff91c62a7231fbcc7051ba7dfd6b
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-17 21:48:44 +0300

    HT14 part 1 corrected 1

--- scripts/script.js
+++ scripts/script.js
@@ -14,13 +14,11 @@ DomElement.prototype.elemCreate = function (strToElem) {
     if (this.selector.charAt(0) === '.') {
         el = document.createElement('div');
         el.className = this.selector.substring(1);
-    } else
-        if (this.selector.charAt(0) === '#') {
-            el = document.createElement('p');
-            el.setAttribute('id', this.selector.substring(1));
-        } else {
-            return;
-        }
+    }
+    if (this.selector.charAt(0) === '#') {
+        el = document.createElement('p');
+        el.setAttribute('id', this.selector.substring(1));
+    }
     el.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`;
     el.textContent = strToElem;
     document.querySelector('body').insertAdjacentElement('afterbegin', el);
